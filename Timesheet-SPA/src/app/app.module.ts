b8f3affc6717ff919c8fbbde73c36523f4ee3907
robin <sio@MBP-de-issam.home>
2019-03-29 09:27:07 +0100

    Initial Commit

--- Timesheet-SPA/src/app/app.module.ts
+++ Timesheet-SPA/src/app/app.module.ts
@@ -4,6 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ClientComponent } from './client/client.component';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const appRoutes: Routes = [
+   {path: 'clients', component: ClientComponent}
+];
 
 @NgModule({
    declarations: [
@@ -12,7 +18,8 @@ import { ClientComponent } from './client/client.component';
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [],
    bootstrap: [
