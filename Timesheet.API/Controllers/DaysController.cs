5d13073519f494db4b17b86c7d8066cf69b6a5db
theo wach <twach@tech-cloud.fr>
2019-03-29 13:05:23 +0100

    update

--- Timesheet.API/Controllers/DaysController.cs
+++ Timesheet.API/Controllers/DaysController.cs
@@ -27,7 +27,7 @@ namespace Timesheet.API.Controllers
             return Ok(days);
         }
 
-        // GET api/days/5
+        // GET api/days/(userid)/(projetid)/(date)
         [HttpGet("{userId}/{projetId}/{date}")]
         public async Task<IActionResult> GetDay(int userId, int projetId, DateTime date)
         {
@@ -37,7 +37,7 @@ namespace Timesheet.API.Controllers
             return Ok(day);
         }
 
-        // POST api/clients
+        // POST api/days
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] Day day)
         {
---
+++ Timesheet.API/Controllers/UserController.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Timesheet.API.Models;
+
+namespace Timesheet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly TimesheetAPIContext _context;
+        public UserController (TimesheetAPIContext context)
+       {
+           _context = context;
+       }
+
+        // GET api/users
+       [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var Users = await _context.Users.ToListAsync();
+
+            return Ok(Users);
+        }
+
+        // GET api/user/(id)
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.ID == id);
+
+            return Ok(user);
+        }
+
+        // POST api/user
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            
+            return Ok();
+        }
+
+        // PUT api/users/(id)
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] User user)
+        {
+            var dbUser = _context.Users.FirstOrDefault(x => x.ID == id);
+            dbUser.Name = user.Name;
+            dbUser.Residence = user.Residence;
+            dbUser.Phone = user.Phone;
+            dbUser.Mail = user.Mail;
+            dbUser.Statut = user.Statut;
+            dbUser.Salary = user.Salary;
+
+            _context.Users.Update(dbUser);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
